6ceefac3c6211f7c6c81f09ef5fe46975346edbd
Sylvain Perron <slvn.perron+github@gmail.com>
2018-02-23 14:31:10 -0500

    Added step-final

---
+++ step-final/src/index.js
@@ -0,0 +1,43 @@
+const _ = require('lodash')
+const jsdoc = require('jsdoc-api')
+
+const renderers = require('./renderers')
+const actions = require('./actions')
+
+module.exports = bp => {
+  ////////////////////////////
+  /// INITIALIZATION
+  ////////////////////////////
+
+  // Register all renderers
+  Object.keys(renderers).forEach(name => {
+    bp.renderers.register(name, renderers[name])
+  })
+
+  jsdoc.explain({ files: [__dirname + '/actions.js'] }).then(docs => {
+    bp.dialogEngine.setFunctionMetadataProvider(fnName => {
+      const meta = docs.find(({ name }) => name === fnName)
+      return {
+        desciption: meta.description,
+        params: (meta.params || [])
+          .filter(({ name }) => name.startsWith('args.'))
+          .map(arg => ({ ...arg, name: arg.name.replace('args.', '') }))
+      }
+    })
+    bp.dialogEngine.registerFunctions(actions)
+  })
+
+  ////////////////////////////
+  /// Conversation Management
+  ////////////////////////////
+
+  bp.hear(/\/forget/i, async (event, next) => {
+    await bp.users.untag(event.user.id, 'nickname')
+    // By not calling next() here, we "swallow" the event (won't be processed by the dialog engine below)
+  })
+
+  // All events that should be processed by the Flow Manager
+  bp.hear({ type: /text|message|quick_reply/i }, (event, next) => {
+    bp.dialogEngine.processMessage(event.sessionId || event.user.id, event).then()
+  })
+}
